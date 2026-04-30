fd1dfe0521538d2d14085bded49d7f036ce3b734
revathivenkatesan20 <revathi2004manju@gmail.com>
2026-04-30 12:55:41 +0530

    chore: add db_host debug to build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -8,5 +8,9 @@ pip install -r requirements.txt
 # Collect static files
 python manage.py collectstatic --no-input
 
+echo "--- DEBUG INFO ---"
+echo "DB_HOST is set to: '${DB_HOST}'"
+echo "------------------"
+
 # Run migrations
 python manage.py migrate
