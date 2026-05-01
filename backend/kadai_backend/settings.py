408cf732617e430f02b4ed19a6d696b3a9100c6b
revathivenkatesan20 <revathi2004manju@gmail.com>
2026-05-01 14:31:35 +0530

    Upgrade to modern STORAGES syntax to ensure Cloudinary persistence on Render

--- backend/kadai_backend/settings.py
+++ backend/kadai_backend/settings.py
@@ -117,13 +117,29 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-# Cloudinary Storage Settings
+# Cloudinary Storage Settings (Modern Django 4.2+ STORAGES syntax)
 if not DEBUG or os.environ.get('CLOUDINARY_CLOUD_NAME'):
-    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+    STORAGES = {
+        "default": {
+            "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+        },
+        "staticfiles": {
+            "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        },
+    }
     CLOUDINARY_STORAGE = {
         'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
         'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
         'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+    }
+else:
+    STORAGES = {
+        "default": {
+            "BACKEND": "django.core.files.storage.FileSystemStorage",
+        },
+        "staticfiles": {
+            "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        },
     }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
